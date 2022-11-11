a9692d117b65683cb91302f0e25b58e4411c8e0a
Devdit <88121395+Devdit@users.noreply.github.com>
2022-11-11 08:28:55 +0700

    Add files via upload

---
+++ array.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+import javax.swing.JOptionPane;
+
+public class array {
+    public static void main(String[] args) {
+        
+       String [][] nama = {{"Alya", "18765"}, {"Aladin", "11876"}, {"Nala", "17622"}, {"Wawan", "29988"}};
+       
+    }
+}
